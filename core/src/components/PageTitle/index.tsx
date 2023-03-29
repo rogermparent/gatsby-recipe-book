67e83d1f4b33d65ca9f68b6ebcee228dc6cd71fc
rogermparent <rogermparent@gmail.com>
2023-03-29 13:30:19 -0400

    Change PageTitle to h1

--- core/src/components/PageTitle/index.tsx
+++ core/src/components/PageTitle/index.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 import * as styles from "./styles.css";
 
 const PageTitle: React.FC<{ children: ReactNode }> = ({ children }) => (
-  <h2 className={styles.pageTitle}>{children}</h2>
+  <h1 className={styles.pageTitle}>{children}</h1>
 );
 
 export default PageTitle;
