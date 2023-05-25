7317f3eea871ffa3172ec40e0194892aa4b426e4
rogermparent <rogermparent@gmail.com>
2023-05-24 22:51:16 -0400

    Remove all usages of sprinkles

--- core/src/components/PageTitle/styles.css.ts
+++
@@ -1,9 +0,0 @@
-import { sprinkles } from "../../styles/sprinkles.css";
-
-export const pageTitle = sprinkles({
-  fontSize: ["2xl", "3xl"],
-  padding: 0,
-  marginX: 2,
-  marginY: [3, 4],
-  fontFamily: "sans",
-});
--- core/src/components/Recipe/styles.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from "@vanilla-extract/css";
-
-export const title = style({
-  fontFamily: "sans",
-  fontSize: "2rem",
-});
