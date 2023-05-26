3974cdb900263329b02635dc3a7e6898161d8f98
rogermparent <rogermparent@gmail.com>
2023-05-26 12:17:30 -0400

    Tweak palette and add light theme support

--- core/src/styles/theme.css.ts
+++ core/src/styles/theme.css.ts
@@ -1,12 +1,32 @@
-import { createGlobalTheme } from "@vanilla-extract/css";
-import { colors } from "./colors.css";
+import {
+  createGlobalTheme,
+  globalStyle,
+  assignVars,
+} from "@vanilla-extract/css";
+import { colors, lightMode } from "./colors.css";
 
 const fonts = {
   body: "sans-serif",
   heading: "serif",
 };
 
-export const vars = createGlobalTheme(":root", {
-  colors,
+export const restVars = createGlobalTheme(":root", {
   fonts,
+});
+
+export const colorVars = createGlobalTheme(":root", {
+  colors,
+});
+
+export const vars = {
+  ...restVars,
+  ...colorVars,
+};
+
+globalStyle(":root", {
+  "@media": {
+    "(prefers-color-scheme: light)": {
+      vars: assignVars(colorVars, { colors: lightMode }),
+    },
+  },
 });
