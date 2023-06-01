299432ce680e661024a20e67c74af2228051ddc4
Roger <rogermparent@gmail.com>
2023-06-01 10:46:34 -0400

    Add copy test and improve related tests and fixtures (#5)
    * Test auto-slug behavior in new recipe test
    
    * Remove null fields from json
    
    * Add copy test and improve edit test

--- editor/cypress/e2e/recipe/edit.test.ts
+++ editor/cypress/e2e/recipe/edit.test.ts
@@ -12,6 +12,9 @@ describe("Recipe Edit Page", () => {
   });
 
   it("Can edit a recipe", () => {
+    const oldTitle = "Test Recipe";
+    const newTitle = "Edited Recipe";
+
     cy.setFixture("single");
     cy.visit("/recipe/edit/test-recipe", {
       timeout: 10000,
@@ -20,13 +23,52 @@ describe("Recipe Edit Page", () => {
     cy.findAllByText("Edit Recipe");
 
     // Fill out and submit edit form
-    cy.get("input[name=name]").clear().type("Edited Test Recipe");
+    cy.get("input[name=name]").clear().type(newTitle);
     cy.findByText("Submit Edit").click();
     cy.waitForRouteChange();
 
     // Edit form submission should redirect to recipe page
     cy.url().assertRoute("/recipe/view/test-recipe");
-    cy.get("body").findByText("Edited Test Recipe", { timeout: 10000 });
+    cy.get("body").findByText(newTitle, { timeout: 10000 });
+
+    // Only the new recipe should exist
+
+    cy.visit("/");
+    cy.waitForRouteChange();
+
+    cy.findByText(newTitle);
+    cy.findByText(oldTitle).should("not.exist");
+  });
+
+  it("Can copy a recipe", () => {
+    const newTitle = "Copied Recipe";
+    const newSlug = "copied-recipe";
+
+    cy.setFixture("single");
+    cy.visit("/recipe/edit/test-recipe", {
+      timeout: 10000,
+    });
+    cy.waitForRouteChange();
+    cy.findAllByText("Edit Recipe");
+
+    // Fill out and submit edit form
+    cy.get("input[name=name]").clear().type(newTitle);
+    cy.findByLabelText("Slug").clear();
+    cy.findByLabelText("Copy").click();
+    cy.findByText("Submit Edit").click();
+    cy.waitForRouteChange();
+
+    // Edit form submission should redirect to recipe page
+    cy.url().assertRoute("/recipe/view/" + newSlug);
+    cy.get("body").findByText(newTitle, { timeout: 10000 });
+
+    // The original recipe and new recipe should both exist
+
+    cy.visit("/");
+    cy.waitForRouteChange();
+
+    cy.findByText("Test Recipe");
+    cy.findByText(newTitle);
   });
 
   it("Can delete a recipe", () => {
