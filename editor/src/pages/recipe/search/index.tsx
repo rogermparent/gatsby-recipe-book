f32f892b23d5122a70498a2630cf341380064bdc
rogermparent <rogermparent@gmail.com>
2023-07-04 17:53:32 -0400

    Add basic highlighting into search results

--- editor/src/pages/recipe/search/index.tsx
+++ editor/src/pages/recipe/search/index.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { graphql, HeadFC, Link, PageProps, useStaticQuery } from "gatsby";
 import SiteLayout from "core/src/components/SiteLayout";
 import { Metadata } from "core/src/components/Metadata";
 import { useFlexSearch } from "react-use-flexsearch";
+
+const BoldMatches = ({ splitInput }: { splitInput: string[] }) =>
+  splitInput.map((text, i) =>
+    i % 2 === 0 ? <Fragment key={i}>{text}</Fragment> : <b key={i}>{text}</b>
+  );
 
 const RecipeSearchPage: React.FC<PageProps<Queries.RecipePageQuery>> = () => {
   const indexQuery = useStaticQuery(graphql`
@@ -19,17 +24,35 @@ const RecipeSearchPage: React.FC<PageProps<Queries.RecipePageQuery>> = () => {
     id: string;
     slug: string;
     name: string;
+    ingredients: string[];
   }[];
+  const queryMatchRegex = new RegExp(`(${query})`, "i");
   return (
     <SiteLayout>
       <div>
         <h1>Search</h1>
         <input onChange={(e) => setQuery(e.target.value)} />
-        {results.map(({ id, slug, name }) => (
-          <div key={id}>
-            <Link to={`/recipe/view/${slug}`}>{name}</Link>
-          </div>
-        ))}
+        {results.map((item) => {
+          const { id, slug, name, ingredients } = item;
+          const splitName = name.split(queryMatchRegex);
+          return (
+            <div key={id}>
+              <Link to={`/recipe/view/${slug}`}>
+                <BoldMatches splitInput={splitName} />
+              </Link>
+              <ul>
+                {ingredients.map((ingredient, ingredientIndex) => {
+                  const splitIngredient = ingredient.split(queryMatchRegex);
+                  return splitIngredient.length > 1 ? (
+                    <li key={ingredientIndex}>
+                      <BoldMatches splitInput={splitIngredient} />
+                    </li>
+                  ) : null;
+                })}
+              </ul>
+            </div>
+          );
+        })}
       </div>
     </SiteLayout>
   );
